639f40a2980bb2cb2b3f39a4239d2c355ef82939
Hausmans <shausman98@gmail.com>
2016-04-28 08:43:00 -0700

    Button
    Button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Button : MonoBehaviour {
 
 	// Use this for initialization
-	ButtonPress button1;
+	ButtonPress button;
 	ButtonPress2 button2;
 	ButtonPress3 button3;
 	public bool step1;
@@ -12,21 +12,23 @@ public class Button : MonoBehaviour {
 	public bool step3;
 
 	void Start (){
-		button1 = GameObject.Find("Button1");
-		button2 = GameObject.Find("Button2");
-		button3 = GameObject.Find("Button3");
+		button = GameObject.Find("Button").getComponent("ButtonPress");
+		button2 = GameObject.Find("Button 1").getComponent("ButtonPress2");
+		button3 = GameObject.Find("Button 2").getComponent("ButtonPress3");
 	}
 	// Update is called once per frame
 	void Update () {
-		if ( button1.button1down&& button2.button2down == false && button3.button3down == false) {
+		if ( button1.button1down && button2.button2down == false && button3.button3down == false) {
 			step1=true;
 		}
-		if (button2.button2down&&step1) {
+		if (button2.button2down && step1) {
 			step2=true;
 		}
-		if (button3.button3down && step1&&step2==false) {
-			step1=false;}
-
+		if (button3.button3down && step1 && step2==false) {
+			step1=false;
+		}
 		if(button3.button3down&&step2){
-			step3 =true;}}
+			step3 =true;
+		}
+	}
 }
