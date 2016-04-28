d486e096f3e613e7e9e29d684d1a7b350c2f39e2
Hausmans <shausman98@gmail.com>
2016-04-28 09:11:22 -0700

    Button

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,31 +4,27 @@ using System.Collections;
 public class Button : MonoBehaviour {
 
 	// Use this for initialization
-	ButtonPress button;
-	ButtonPress2 button2;
-	ButtonPress3 button3;
+	ButtonPress button1;
+	ButtonPress button2;
+	ButtonPress button3;
 	public bool step1;
 	public bool step2;
 	public bool step3;
-
 	void Start (){
-		button = GameObject.Find("Button").getComponent("ButtonPress");
-		button2 = GameObject.Find("Button 1").getComponent("ButtonPress2");
-		button3 = GameObject.Find("Button 2").getComponent("ButtonPress3");
+		button1 = GameObject.Find("button").GetComponent<ButtonPress>();
+		button2 = GameObject.Find("button 1").GetComponent<ButtonPress>();
+		button3 = GameObject.Find("button 2").GetComponent<ButtonPress>();
 	}
 	// Update is called once per frame
 	void Update () {
-		if ( button1.button1down && button2.button2down == false && button3.button3down == false) {
-			step1=true;
-		}
-		if (button2.button2down && step1) {
-			step2=true;
-		}
-		if (button3.button3down && step1 && step2==false) {
-			step1=false;
-		}
-		if(button3.button3down&&step2){
+		if ( button1.button1down&& button2.button1down == false && button3.button1down == false) {
+			step1=true;}
+		if (button2.button1down&&step1) {
+			step2=true;}
+		if (button3.button1down && step1&&step2==false) {
+			step1=false;}
+		if(button3.button1down&&step2){
 			step3 =true;
-		}
+			this.GetComponent<Animator>().SetBool("Door", true);}
 	}
 }
